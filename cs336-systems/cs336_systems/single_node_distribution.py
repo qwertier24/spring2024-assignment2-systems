ae3ae53ac3f519b7d14fed9bd9bad94c016dd554
Yukun Ma <gmayukun@gmail.com>
2024-06-02 08:33:13 -0700

    modify single node distribution impl

--- cs336-systems/cs336_systems/single_node_distribution.py
+++ cs336-systems/cs336_systems/single_node_distribution.py
@@ -3,6 +3,7 @@ import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
 import argparse
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--backend", type=str)
@@ -20,6 +21,12 @@ def setup(rank, world_size):
 
 def distributed_demo(rank, world_size):
     setup(rank, world_size)
+    # warm up
+
+    data = torch.rand((256, args.data_size_kb)).to(args.device)
+    dist.all_reduce(data, async_op=False)
+    torch.cuda.synchronize()
+
     data = torch.rand((256, args.data_size_kb)).to(args.device)
     print(f"rank {rank} data (before all-reduce): {torch.mean(data)}")
     t1 = time.time()
@@ -28,6 +35,11 @@ def distributed_demo(rank, world_size):
     t2 = time.time()
     print(f"rank {rank} data (after all-reduce): {torch.mean(data)}")
     print(f"time {rank}: ", t2-t1)
+
+    gathered_times = [None] * world_size
+    dist.all_gather_object(gathered_times, t2 - t1)
+    if rank == 0:
+        print(f"average time:", sum(gathered_times) / len(gathered_times))
 
 
 def main():
